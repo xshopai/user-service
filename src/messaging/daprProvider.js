7a05536adc0a4ba28491975721e4f8eb6dc6ebd8
prasadhonrao <honrao.prasad@gmail.com>
2026-02-04 14:57:41 +0000

    feat: remove rawPayload metadata - standardize CloudEvents

--- src/messaging/daprProvider.js
+++ src/messaging/daprProvider.js
@@ -79,6 +79,8 @@ class DaprProvider extends MessagingProvider {
         eventData.metadata = { correlationId };
       }
 
+      // Let Dapr handle CloudEvents wrapping/unwrapping natively
+      // Do NOT use rawPayload - it causes deserialization issues with Azure Service Bus
       await client.pubsub.publish(this.pubsubName, topic, eventData);
 
       logger.info('Published event via Dapr', null, {
